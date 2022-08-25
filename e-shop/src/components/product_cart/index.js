2fe715bd9e3b32ace3dd14e4f49fd0eca2ae5411
syahrul25arul <syahrul25arul@gmail.com>
2022-08-25 09:30:55 +0700

    hilngkan button ad to cart di section

--- e-shop/src/components/product_cart/index.js
+++ e-shop/src/components/product_cart/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Button, ImageProduct, ItemInfo, ItemPrice } from "./ProductCart.styles";
 import { products } from "../../Images";
 import ItemRating from "../items_rating";
-import { ButtonAddToCart } from "../button";
 import { useNavigate } from "react-router-dom";
 
 function Index({ product_name, price, image, rating, id }) {
@@ -22,8 +21,6 @@ function Index({ product_name, price, image, rating, id }) {
 				<ItemPrice className="item-price">
 					<span>Rp. {price}</span>
 				</ItemPrice>
-
-				<ButtonAddToCart className={`btn-add-to-cart`}>Add to cart</ButtonAddToCart>
 			</ItemInfo>
 		</div>
 	);
